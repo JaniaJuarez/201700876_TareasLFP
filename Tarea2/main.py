02cfa09c3ffc154b3f4779d396a0c94b9fc90294
jania <janiajuarez@gmail.com>
2020-08-31 16:11:47 -0600

    commit si

--- Tarea2/main.py
+++ Tarea2/main.py
@@ -4,7 +4,8 @@ def readJson():
     file = open('tarea2.json',)
     data = json.load(file)
     file.close()
-    print(data)
+    print("El tipo es")
+    print(type(data))
     return data
 
 dict = readJson()
@@ -15,20 +16,25 @@ from xml.dom import minidom
 doc = minidom.parse('tarea2.xml',)
 personas = doc.getElementsByTagName("persona")
 for persona in personas:
-    satributo = persona.getAttribute("atributo")
+    atributo = persona.getAttribute("atributo")
     edad = persona.getElementsByTagName("edad")[0]
     menordeedad = persona.getElementsByTagName("menordeedad")[0]
     estatura = persona.getElementsByTagName("estatura")[0]
-    print("atributo:%s " % satributo)
+    print("El tipo es")
+    print(type(persona))
+    print("atributo:%s " % atributo)
     print("edad:%s" % edad.firstChild.data)
     print("menordeedad:%s" % menordeedad.firstChild.data)
     print("estatura:%s" % estatura.firstChild.data)
+
 
 import csv
 
 with open('tarea2.csv') as f:
     reader = csv.reader(f)
     for row in reader:
+        print("El tipo es")
+        print(type(row))
         print("registro: {0},edad:{1},menordeedad:{2},estatura:{3}".format(row[0],row[1],row[2],row[3]))
 
 
--- Tarea4/lista.py
+++ Tarea4/lista.py
@@ -1,2 +1,14 @@
+
 import webbrowser
-webbrowser.open("file:///C:/Users/jania/OneDrive/Escritorio/Laboratorio/Repositorio/201700876_TareasLFP/Tarea4/Template/lista.html", new=2, autoraise=True)
+webbrowser.open("file:///C:/Users/jania/OneDrive/Escritorio/Laboratorio/Repositorio/201700876_TareasLFP/Tarea4/templates/template.html", new=2, autoraise=True)
+
+def creador_usuario(nombre, fecha, activo,saldo):
+
+    usuario = {
+        "nombre": nombre,
+        "edad": edad,
+        "activo": activo,
+        "saldo": saldo,
+        
+    }
+    return usuario
