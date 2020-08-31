7f2d809c11f6d3115ed6b344a5fe4322542446af
jania <janiajuarez@gmail.com>
2020-08-31 16:12:37 -0600

    Hoy si

--- Tarea4/lista.py
+++ Tarea4/lista.py
@@ -2,7 +2,7 @@
 import webbrowser
 webbrowser.open("file:///C:/Users/jania/OneDrive/Escritorio/Laboratorio/Repositorio/201700876_TareasLFP/Tarea4/templates/template.html", new=2, autoraise=True)
 
-def creador_usuario(nombre, fecha, activo,saldo):
+def creador_usuario(nombre, edad, activo,saldo):
 
     usuario = {
         "nombre": nombre,
